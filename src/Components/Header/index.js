b006880d93d92575d6fd7179c7930e7a8bb8300c
Dayanne Alcantara <dayannealcantara14@gmail.com>
2021-07-24 22:20:59 -0300

    create header

---
+++ src/Components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css';
+import Logo from '../../assets/imagens/Log.png'
+
+const Header = () => {
+  return (
+    <div class="header">
+      <img src={Logo} alt="" />
+      
+    </div>
+  )
+}
+
+export default Header
